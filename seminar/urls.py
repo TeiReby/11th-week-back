e1c2c29e500a17d3cebe686ccd4754b66f6f1d7a
bona7 <144210921+bona7@users.noreply.github.com>
2025-06-05 23:01:10 +0900

    test deploy

--- seminar/urls.py
+++ seminar/urls.py
@@ -39,6 +39,6 @@ urlpatterns = [
     path('api/account/', include('account.urls')),
     path('api/tag/', include('tag.urls')),
     path('api/comment/', include('comment.urls')),
-    # path('api/test-deploy/', views.test_deploy, name='test-deploy'),
+    path('api/test-deploy/', views.test_deploy, name='test-deploy'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
