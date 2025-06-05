b02d6b038ded73e26e38af8ba98783f747a84618
teireby <dongin1001@snu.ac.kr>
2025-06-05 21:03:30 +0900

    백 수정1

--- seminar/settings.py
+++ seminar/settings.py
@@ -69,11 +69,8 @@ CSRF_TRUSTED_ORIGINS = [
    'http://127.0.0.1:3000', 
    'http://localhost:3000',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS= [ # (헤더) Access-Control-Allow-Origin 에 담을 주소들
-  'http://127.0.0.1:3000', 
-  'http://localhost:3000',
-]
 CORS_ALLOW_CREDENTIALS = True # cookie를 주고받으려면 얘를 True로 설정해야 해요.
 CORS_ALLOW_HEADERS = (
     "accept",
